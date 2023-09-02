ab602bd680ac7bab08faac06956997204287499e
Toufiqur Rahman <toufiqurraa@gmail.com>
2023-09-03 00:19:37 +0600

    Time: 0 ms (100.00%) | Memory: 8.7 MB (34.80%) - LeetSync

---
+++ 66-plus-one/plus-one.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+       vector<int> ans;
+       int carry = 0;
+       int digitsSize = digits.size() - 1;
+
+       for(int i = digitsSize; i >= 0; i--) {
+           int sum;
+
+           if(i == digitsSize) {
+               sum = digits[i] + 1;   
+           } else {
+               sum = digits[i] + carry;
+           }
+
+           if(digitsSize == 0 && sum > 9) {
+               carry = 0;
+               ans.push_back(1);
+               ans.push_back(0);
+           } else {
+                if(sum > 9) {
+                    carry = sum / 10;
+                    sum = sum % 10;
+                } else {
+                    carry = 0;
+                }
+
+                ans.insert(ans.begin(), sum);
+            }
+       }
+
+       if(carry) {
+           ans.insert(ans.begin(), carry);
+       }
+
+       return ans;
+    }
+};
