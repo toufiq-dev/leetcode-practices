0046cb1ceae7cd8ab1e03c451e6a2cffa9acd3b0
Toufiqur Rahman <146459499+toufiq-dev@users.noreply.github.com>
2023-10-15 11:32:40 +0600

    Time: 12 ms (71.18%), Space: 20.2 MB (5.28%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,17 +1,35 @@
 class Solution {
 public:
+    // Optimal solution
+    // TC: O(N)
+    // SC: O(1)
     int majorityElement(vector<int>& nums) {
-        unordered_map<int, int> frequency;
+        int candidate = -1, votes = 0;
+
+        // find majority candidate
+        for(int i = 0; i < nums.size(); i++) {
+            if(votes == 0) {
+                candidate = nums[i];
+                votes = 1;
+            } else {
+                if(nums[i] == candidate) {
+                    votes++;
+                } 
+                else 
+                    votes--;
+            }
+        }
+        
+        // find if majority candidate appears more than ⌊n / 2⌋ times.
+        int count = 0;
 
         for(int i = 0; i < nums.size(); i++) {
-            if(frequency.count(nums[i]) == 0) frequency[nums[i]];
-
-            frequency[nums[i]]++;
+            if(nums[i] == candidate)
+                count++;
         }
 
-        for(auto x : frequency) {
-            if(x.second > floor(nums.size() / 2)) return x.first;
-        }
+        if(count > floor(nums.size() / 2)) 
+            return candidate;
 
         return -1;
     }
