d67d41d809902c3e3219c4355252747c997ba651
Toufiqur Rahman <146459499+toufiq-dev@users.noreply.github.com>
2023-10-17 00:40:14 +0600

    Time: 76 ms (97.94%), Space: 24.3 MB (72.58%) - LeetHub

---
+++ 0015-3sum/0015-3sum.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    // TC: O(N)
+    // SC: O(N)
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        // step1: sort(nums)
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> triplets;
+
+        // step2: iterate through nums,
+        // for every nums member run two sum operation
+        // igonre duplicates
+        for(int i = 0; i < nums.size(); i++) {
+            int a = nums[i];
+
+            // for every a(prefix), find b, c form  
+            // suffix such that, b + c == -a
+            int L = i + 1;
+            int R = nums.size() - 1;
+            int target = -a;
+            while(L < R) {
+                int b = nums[L];
+                int c = nums[R];
+                if(b + c < target) 
+                    L++;
+                else if(b + c > target)
+                    R--;
+                else {
+                    while(L + 1 < R && nums[L + 1] == b) 
+                        L++;
+                    while(R - 1 > L  && nums[R - 1] == c)
+                        R--;
+
+                    triplets.push_back({a, b, c});
+                    L++;
+                    R--;
+                }
+            }
+
+            while(i + 1 < nums.size() && nums[i + 1] == a)
+                i++;
+        }
+
+        return triplets;
+    }
+};
