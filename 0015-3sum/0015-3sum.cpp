bb1f3eeed95006edef2565abf5122a9e5a85df66
Toufiqur Rahman <146459499+toufiq-dev@users.noreply.github.com>
2023-10-18 10:17:20 +0600

    Time: 111 ms (68.35%), Space: 24.3 MB (80.34%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,46 +1,45 @@
 class Solution {
-public:
-    // TC: O(N ^ 2)
-    // SC: O(N)
-    vector<vector<int>> threeSum(vector<int>& nums) {
-        // step1: sort(nums)
-        sort(nums.begin(), nums.end());
-        vector<vector<int>> triplets;
+    public:
+        // TC: O(N)
+        // SC: O(N)
+        vector<vector<int>> threeSum(vector<int>& nums) {
+            // step1: sort the array
+            sort(nums.begin(), nums.end());
 
-        // step2: iterate through nums,
-        // for every nums member run two sum operation
-        // igonre duplicates
-        for(int i = 0; i < nums.size(); i++) {
-            int a = nums[i];
+            vector<vector<int>> triplets;
 
-            // for every a(prefix), find b, c form  
-            // suffix such that, b + c == -a
-            int L = i + 1;
-            int R = nums.size() - 1;
-            int target = -a;
-            while(L < R) {
-                int b = nums[L];
-                int c = nums[R];
-                if(b + c < target) 
-                    L++;
-                else if(b + c > target)
-                    R--;
-                else {
-                    while(L + 1 < R && nums[L + 1] == b) 
+            // step2: for each element in array apply two-sum
+            for(int i = 0; i < nums.size(); i++) {
+                int a = nums[i];
+
+                // for every a(prefix), find b, c form  
+                // suffix such that, b + c == -a
+                int L = i + 1;
+                int R = nums.size() - 1;
+                int target = -a;
+
+                while(L < R) {
+                    if(nums[L] + nums[R] < target)
                         L++;
-                    while(L < R - 1 && nums[R - 1] == c)
+                    else if (nums[L] + nums[R] > target) 
                         R--;
+                    else {
+                        // remove duplicates
+                        while(L < R && nums[L] == nums[L + 1])
+                            L++;
+                        while(L < R - 1 && nums[R] == nums[R - 1])
+                            R--;
 
-                    triplets.push_back({a, b, c});
-                    L++;
-                    R--;
+                        triplets.push_back({nums[i], nums[L], nums[R]});
+                        L++;
+                        R--;
+                    }                        
                 }
+
+                while(i + 1 < nums.size() && nums[i] == nums[i + 1])
+                    i++;
             }
 
-            while(i + 1 < nums.size() && nums[i + 1] == a)
-                i++;
+            return triplets;
         }
-
-        return triplets;
-    }
 };
