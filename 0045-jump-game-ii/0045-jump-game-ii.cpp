221eb24bf2574adcff49ac7cff224172d42f2b7c
Toufiqur Rahman <146459499+toufiq-dev@users.noreply.github.com>
2023-12-07 12:22:59 +0600

    Time: 12 ms (69.34%), Space: 16.9 MB (76.04%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        if(nums.size() == 1) return 0;
+
+        int max_jump = 0;
+        int curr_end = 0;
+        int total_jump = 0;
+        for(int i = 0; i < nums.size() - 1; ++i) {
+            max_jump = max(max_jump, nums[i] + i);
+            if(i == curr_end) {
+                ++total_jump;
+                curr_end = max_jump;
+            }
+        }
+
+        return total_jump;
+    }
+};
