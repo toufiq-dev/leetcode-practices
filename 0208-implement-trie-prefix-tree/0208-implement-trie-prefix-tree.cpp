7c1641d686567d853f5e4e35023a119bec845211
Toufiqur Rahman <146459499+toufiq-dev@users.noreply.github.com>
2024-01-13 02:15:10 +0600

    Time: 39 ms (96.13%), Space: 43.8 MB (58.9%) - LeetHub

---
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.cpp
@@ -0,0 +1,69 @@
+class Trie {
+public:
+    class Node {
+    public: 
+        const static int MAX_CHAR = 26;
+        int cnt;
+        bool isEnd;
+        Node* next[MAX_CHAR];
+
+        Node() {
+            cnt = 0;
+            isEnd = false;
+            for(int i = 0; i < MAX_CHAR; i++) {
+                next[i] = nullptr;
+            }
+        }
+    };
+
+    Node* root;
+
+    Trie() {
+        root = new Node();
+    }
+    
+    // TC: O(M)
+    // TC: O(M)
+    void insert(string word) {
+        auto curr = root;
+        for(char &c : word) {
+            int rnk = c - 'a';
+            if(curr -> next[rnk] == nullptr) {
+                curr -> next[rnk] = new Node();
+            }
+
+            curr = curr -> next[rnk];
+            curr -> cnt++;
+        }
+        curr -> isEnd = true;
+    }
+    
+    // TC: O(M)
+    // SC: O(1)
+    bool search(string word, bool isPrefixSearch = false) {
+        auto curr = root;
+        for(char &c : word) {
+            int rnk = c - 'a';
+            if(curr -> next[rnk] == nullptr) {
+                return false;
+            }
+
+            curr = curr -> next[rnk];
+        }
+        return isPrefixSearch || curr && curr -> isEnd;
+    }
+    
+    // TC: O(M)
+    // SC: O(1)
+    bool startsWith(string prefix) {
+        return search(prefix, true);
+    }
+};
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie* obj = new Trie();
+ * obj->insert(word);
+ * bool param_2 = obj->search(word);
+ * bool param_3 = obj->startsWith(prefix);
+ */
