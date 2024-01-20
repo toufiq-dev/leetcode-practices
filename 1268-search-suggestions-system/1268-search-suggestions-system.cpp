64d022273a83041a30997d98863c7b3086033a98
Toufiqur Rahman <146459499+toufiq-dev@users.noreply.github.com>
2024-01-20 20:20:41 +0600

    Time: 79 ms (62.33%), Space: 75.7 MB (54.22%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.cpp
+++ 1268-search-suggestions-system/1268-search-suggestions-system.cpp
@@ -1,35 +1,71 @@
 class Solution {
 public:
-    bool startsWith(string &str, string&pref) {
-        if (str.size() < pref.size())
-            return false;
-        
-        for (int i = 0; i < pref.size(); ++i) {
-            if(pref[i] != str[i])
-                return false;
-        }
-
-        return true;
-    }
-    vector<vector<string>> suggestedProducts(vector<string>& products, string searchWord) {
-        sort(products.begin(), products.end());
-        vector<vector<string>> results;
-
-        string pref = "";
-        for (auto &ch : searchWord) {
-            pref.push_back(ch);
-            vector<string> result;
-
-            for (auto &product: products) {
-                if (startsWith(product, pref)) 
-                    result.push_back(product);
-                if(result.size() == 3) 
-                    break;
+    class TrieNode {
+        public: 
+            bool isEnd;
+            TrieNode* next[26];
+            TrieNode() {
+                for (int i = 0; i < 26; ++i) {
+                    next[i] = nullptr;
+                }
+                isEnd = false;
             }
-            
-            results.push_back(result);
+    };
+    class Trie {
+        public: 
+            TrieNode* root;
+            Trie() {
+                root = new TrieNode();
+            }
+            void insert(string &word) {
+                auto curr = root;
+                for (auto &ch : word) {
+                    int rnk = ch - 'a';
+                    if (curr -> next[rnk] == nullptr) {
+                        curr -> next[rnk] = new TrieNode();
+                    }
+                    curr = curr -> next[rnk];
+                }
+                curr -> isEnd = true;
+            }
+    };
+
+    Trie* buildTrie(vector<string> &products) {
+        Trie* trie = new Trie();
+        for (auto &product : products) {
+            trie -> insert(product);
+        }
+        return trie;
+    }
+
+    void dfs (TrieNode* root, string &prefix, vector<string> &result) {
+        if (root == nullptr || result.size() == 3) return;
+        if (root -> isEnd) {
+            result.push_back(prefix);
+        }
+        for (int i = 0; i < 26; i++) {
+            if (root -> next[i] == nullptr) continue;
+            prefix.push_back((char)(i + 'a'));
+            dfs(root -> next[i], prefix, result);
+            prefix.pop_back();
+        }
+    }
+
+    vector<vector<string>> suggestedProducts(vector<string>& products, string searchWord) {
+        auto trie = buildTrie(products);
+        vector<vector<string>> suggestions;
+
+        auto currNode = trie -> root;
+        string prefix = "";
+
+        for (auto &ch : searchWord) {
+            prefix.push_back(ch);
+            currNode = currNode ? currNode -> next[ch - 'a'] : nullptr;
+            vector<string> result;
+            dfs(currNode, prefix, result);
+            suggestions.push_back(result);
         }
 
-        return results;
+        return suggestions;
     }
 };
