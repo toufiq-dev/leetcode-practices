39f7591a17525af10f735bc88a8293243c1f7f9e
Toufiqur Rahman <146459499+toufiq-dev@users.noreply.github.com>
2024-02-18 23:52:27 +0600

    Time: 19 ms (41.46%), Space: 26.4 MB (45.56%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -13,29 +13,29 @@ class Solution {
 public:
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
         int preorderIndex = 0;
-        TreeNode* ans = treeBuilder(preorder, inorder, preorderIndex, 0, inorder.size() - 1);
+        map<int, int> nodeToIndex;
+        createMapping(nodeToIndex, inorder);
+        TreeNode* ans = treeBuilder(preorder, inorder, preorderIndex, 0, inorder.size() - 1, nodeToIndex);
         return ans;
     }
 private:
     TreeNode* treeBuilder(vector<int>& preorder, vector<int>& inorder, int& preorderIdx, 
-    int inorderStart, int inorderEnd) {
+    int inorderStart, int inorderEnd, map<int, int>& nodeToIndex) {
         // base case
         if (preorderIdx > preorder.size() - 1 || inorderStart > inorderEnd)
             return nullptr;
         
         int element = preorder[preorderIdx++];
         TreeNode* root = new TreeNode(element);
-        int position = findPosition(element, inorder);
-        root -> left = treeBuilder(preorder, inorder, preorderIdx, inorderStart, position - 1);
-        root -> right = treeBuilder(preorder, inorder, preorderIdx, position + 1, inorderEnd);
+        int position = nodeToIndex[element];
+        root -> left = treeBuilder(preorder, inorder, preorderIdx, inorderStart, position - 1, nodeToIndex);
+        root -> right = treeBuilder(preorder, inorder, preorderIdx, position + 1, inorderEnd, nodeToIndex);
 
         return root;
     }
-    int findPosition(int element, vector<int>& inorder) {
+    void createMapping(map<int, int>& nodeToIndex, vector<int>& inorder) {
         for (int i = 0; i < inorder.size(); i++) {
-            if (inorder[i] == element) 
-                return i;
+            nodeToIndex[inorder[i]] = i;
         }
-        return -1;
     }
 };
