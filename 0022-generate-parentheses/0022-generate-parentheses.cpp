944792ede6576de367de8db24f995630214cf5e1
Toufiqur Rahman <146459499+toufiq-dev@users.noreply.github.com>
2024-03-14 15:42:54 +0600

    Time: 3 ms (60.91%), Space: 15.3 MB (43.98%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<string> generateParenthesis(int n) {
+        vector<string> result;
+        string curr;
+        backtrack(result, curr, 0, 0, n);
+        return result;
+    }
+
+private:
+    void backtrack(vector<string>& result, string curr, int open, int close, int max) {
+        if (curr.length() == max * 2) {
+            result.push_back(curr);
+            return;
+        }
+
+        if (open < max)
+            backtrack(result, curr + "(", open + 1, close, max);
+        if (close < open)
+            backtrack(result, curr + ")", open, close + 1, max);
+    }
+};
