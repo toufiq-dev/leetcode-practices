d50236bd20fd06701908d953929e0a1c775e238d
Toufiqur Rahman <146459499+toufiq-dev@users.noreply.github.com>
2024-04-02 19:46:21 +0600

    Time: 39 ms (75.17%), Space: 10.6 MB (37.47%) - LeetHub

---
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int numSquares(int n) {
+        vector<int> cache(n + 1, INT_MAX / 2);
+        cache[0] = 0;
+
+        for (int curr = 1; curr <= n; curr++) {
+            for (int i = 1; i * i <= curr; i++) {
+                cache[curr] = min(cache[curr], 1 + cache[curr - (i * i)]);
+            }
+        }
+
+        return cache[n];
+    }
+};
