9e4da04a97768c4ff5b806a51b2371ac8398fb66
Toufiqur Rahman <146459499+toufiq-dev@users.noreply.github.com>
2024-04-03 15:57:28 +0600

    Time: 81 ms (34.02%), Space: 11.3 MB (22.52%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -1,15 +1,21 @@
 class Solution {
 public:
-    int numSquares(int n) {
-        vector<int> cache(n + 1, INT_MAX / 2);
-        cache[0] = 0;
+    vector<int> cache;
+    int countSquares(int n) {
+        if (n == 0) return 0;
+        if (cache[n] != -1) return cache[n];
 
-        for (int curr = 1; curr <= n; curr++) {
-            for (int i = 1; i * i <= curr; i++) {
-                cache[curr] = min(cache[curr], 1 + cache[curr - (i * i)]);
-            }
+        int res = INT_MAX / 2;
+        for (int i = 1; i * i <= n; i++) {
+            res = min(res, 1 + countSquares(n - (i * i)));
         }
+        cache[n] = res;
 
-        return cache[n];
+        return res;
+    }
+    int numSquares(int n) {
+        cache.clear();
+        cache.resize(n + 1, -1);
+        return countSquares(n);    
     }
 };
