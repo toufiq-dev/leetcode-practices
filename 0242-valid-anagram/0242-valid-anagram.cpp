3d7f5c8d4eadd01447dd2c362cf4f2024030a503
Toufiqur Rahman <146459499+toufiq-dev@users.noreply.github.com>
2024-12-06 11:01:06 +0600

    Time: 0 ms (100%), Space: 9.7 MB (41.54%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        vector<int> fs(256, 0);
-        for(auto &c : s) {
-            ++fs[c];
+        if (s.length() != t.length())
+            return false;
+        
+        vector<int> count(26, 0);
+        for (int i = 0; i < s.length(); i++) {
+            count[s[i] - 'a']++;
+            count[t[i] - 'a']--;
         }
 
-        vector<int> ft(256, 0);
-        for(auto &c : t) {
-            ++ft[c];
+        for (int val : count) {
+            if (val != 0)
+                return false;
         }
 
-        return fs == ft;
+        return true;
     }
 };
