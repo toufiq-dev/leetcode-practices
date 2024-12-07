37dcafc27d3c060b8d8a4326f89cea430ee70fe9
Toufiqur Rahman <146459499+toufiq-dev@users.noreply.github.com>
2024-12-07 21:34:19 +0600

    Time: 0 ms (100%), Space: 72.8 MB (15.59%) - LeetHub

--- 1231-replace-elements-with-greatest-element-on-right-side/1231-replace-elements-with-greatest-element-on-right-side.cpp
+++ 1231-replace-elements-with-greatest-element-on-right-side/1231-replace-elements-with-greatest-element-on-right-side.cpp
@@ -1,25 +1,14 @@
 class Solution {
 public:
-    int findHighest (int index, vector<int>& arr) {
-        int highest = arr[index];
-        for (int i = index + 1; i < arr.size(); i++) {
-            if (highest < arr[i])
-                highest = arr[i];
-        }
-
-        return highest;
-    }
-
     vector<int> replaceElements(vector<int>& arr) {
-        if (arr.size() == 1)
-            return {-1};
-        
-        for (int i = 0; i < arr.size() - 1; i++) {
-            int highest = findHighest(i + 1, arr);
-            arr[i] = highest;
+        int rightMax = -1;
+
+        for (int i = arr.size() - 1; i >= 0; i--) {
+            int newMax = max(rightMax, arr[i]);
+            arr[i] = rightMax;
+            rightMax = newMax;
         }
 
-        arr[arr.size() - 1] = -1;
         return arr;
     }
 };
